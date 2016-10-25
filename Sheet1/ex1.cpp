c9ad68aee2ddbfed3a702ae7e939849b328bcca9
maxtheilig <max.theilig@live.de>
2016-10-25 11:37:37 +0200

    changed output-printing

--- Sheet1/ex1.cpp
+++ Sheet1/ex1.cpp
@@ -6,13 +6,13 @@ int main()
     double analytic_result = 5.0/6.0;
     double result, standartDeviation;
     std::cout << "\n";
-    for(unsigned int N=1000; N<=10000000; N *= 10)
+    for(int N=1000; N<=10000000; N *= 10)
     {
-        std::cout << "# of MC-steps = " << N << std::endl;
+        print("# of MC-steps", N);
         monteCarloIntegration(result, f1, N, standartDeviation);
-        std::cout << "result = " << result << std::endl;
-        std::cout << "numerical - analytic = " << std::abs(result - analytic_result) << std::endl;
-        std::cout << "standart_deviation = " << standartDeviation << std::endl;
+        print("result", result);
+        print("numerical - analytic", std::abs(result - analytic_result));
+        print("standart_deviation", standartDeviation);
         std::cout << "\n";
     }
     return 0;
